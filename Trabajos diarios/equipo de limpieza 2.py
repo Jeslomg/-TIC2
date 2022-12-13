bc404f0600a7a436985b6af7f7d436e0015d1751
Jeslomg <118819774+Jeslomg@users.noreply.github.com>
2022-12-13 13:25:56 +0100

    Delete Trabajos diarios directory

--- Trabajos diarios/equipo de limpieza 2.py
+++
@@ -1,5 +0,0 @@
-### Mostrar mensajes por pantalla ####################################
-print("¡Hola, mundo!") 
-print("Este es tu primer programa con Python")
-print(1,2,3,4,5)
-print(1,2,3,4,5, sep=' y ')
