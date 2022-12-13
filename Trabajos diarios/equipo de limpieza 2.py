5a4bed8e20bddac60890de85c5709b9fa39cdf16
Jeslomg <118819774+Jeslomg@users.noreply.github.com>
2022-12-13 12:46:23 +0100

    Add files via upload

---
+++ Trabajos diarios/equipo de limpieza 2.py
@@ -0,0 +1,5 @@
+### Mostrar mensajes por pantalla ####################################
+print("¡Hola, mundo!") 
+print("Este es tu primer programa con Python")
+print(1,2,3,4,5)
+print(1,2,3,4,5, sep=' y ')
